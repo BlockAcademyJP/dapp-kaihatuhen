5429a3de203856e4263e05bf4ef4a9c32e1bb60e
REGO350 <riokanehiro228pa@gmail.com>
2021-09-17 12:19:32 +0900

    part7

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,11 +1,32 @@
 let buyMode = true;
-let web3, user;
+let token = undefined;
+let web3, user, dexInst, tokenInst;
 let priceData;
+let finalInput, finalOutput;
+
+const linkAddr = "0xDa4A07281aCFc7D8C83E9B1062d0F5389a3C2009";
+const daiAddr = "0xD2e3658987db3Dd37E667fB00706aFae05eaB627";
+const compAddr = "0xA51ceEBf5E8a865dD5e6bc88840b71Ed09ae85e0";
+const dexAddr = "0x22d586899e3d082fCEe040Ac924E6eA8272D98Be";
 
 $(document).on('click', ".dropdown-menu li a", function () {
   let element = $(this);
   let img = element[0].firstElementChild.outerHTML;
   let text = $(this).text();
+  token = text.replace(/\s/g, "");
+  if(user){
+    switch(token){
+      case "DAI":
+        tokenInst = new web3.eth.Contract(abi.token, daiAddr, {from : user});
+        break;
+      case "LINK":
+        tokenInst = new web3.eth.Contract(abi.token, linkAddr, {from : user});
+        break
+      case "COMP":
+        tokenInst = new web3.eth.Contract(abi.token, compAddr, {from : user});
+        break;
+    }
+  }
   $(".input-group .btn").html(img + text);
   $(".input-group .btn").css("color", "#fff");
   $(".input-group .btn").css("font-size", "large");
@@ -25,6 +46,7 @@ $(".btn.login").click(async () => {
       method: "eth_requestAccounts",
     });
     user = accounts[0];
+    dexInst = new web3.eth.Contract(abi.dex, dexAddr, { from: user });
     $(".btn.login").html("Connected");
     $(".btn.swap").html("Enter an amount");
     $("#username").html(user);
@@ -33,8 +55,15 @@ $(".btn.login").click(async () => {
   }
 })
 
-$("#swap-box").submit((e)=>{
+$("#swap-box").submit(async (e)=>{
   e.preventDefault();
+
+  try{
+    buyMode ? await buyToken() : await sellToken()
+  } catch (err){
+    alert(err.message);
+  }
+
 })
 
 $("#arrow-box h2").click(()=>{
@@ -45,6 +74,14 @@ $("#arrow-box h2").click(()=>{
     buyMode = true;
     buyTokenDisplay();
   }
+});
+
+$("#input").on("input", async function () {
+  if(token === undefined){
+    return;
+  }
+  const input = parseFloat($(this).val());
+  await updateOutput(input);
 });
 
 async function getPrice(){
@@ -67,4 +104,92 @@ async function getPrice(){
     linkEth: linkData.chainlink.eth,
     compEth: compData["compound-governance-token"].eth
   }
+}
+
+async function updateOutput(input){
+  let output;
+  switch(token){
+    case "COMP":
+      output = buyMode ? input / priceData.compEth : input * priceData.compEth;
+      break;
+    case "LINK":
+      output = buyMode ? input / priceData.linkEth : input * priceData.linkEth;
+      break;
+    case "DAI":
+      output = buyMode ? input / priceData.daiEth : input * priceData.daiEth;
+      break;
+  }
+  const exchangeRate = output / input;
+  if(output === 0 || isNaN(output)){
+    $("#output").val("");
+    $(".rate.value").css("display", "none");
+    $(".btn.swap").html("Enter an amount");
+    $(".btn.swap").addClass("disabled");
+  }else{
+    $("#output").val(output.toFixed(7));
+    $(".rate.value").css("display", "block");
+    if (buyMode) {
+      $("#top-text").html("ETH");
+      $("#bottom-text").html(" " + token);
+      $("#rate-value").html(exchangeRate.toFixed(5));
+    } else {
+      $("#top-text").html(token);
+      $("#bottom-text").html(" ETH");
+      $("#rate-value").html(exchangeRate.toFixed(5));
+    }
+    await checkBalance(input);
+    finalInput = web3.utils.toWei(input.toString(), "ether");
+    finalOutput = web3.utils.toWei(output.toString(), "ether");
+  }
+}
+
+async function checkBalance(input){
+  const balanceRaw = buyMode 
+    ? await web3.eth.getBalance(user) 
+    : await tokenInst.methods.balanceOf(user).call()
+  const balance = parseFloat(web3.utils.fromWei(balanceRaw, "ether"));
+
+  if(balance >= input){
+    $(".btn.swap").removeClass("disabled");
+    $(".btn.swap").html("Swap");
+  }else {
+    $(".btn.swap").addClass("disabled");
+    $(".btn.swap").html(`Insufficient ${buyMode ? "ETH" : token} balance`);
+  }
+}
+
+function buyToken(){
+  const tokenAddr = tokenInst._address;
+  return new Promise((resolve, reject) => {
+    dexInst.methods
+      .buyToken(tokenAddr, finalInput, finalOutput)
+      .send({ value: finalInput })
+      .then((receipt) => {
+        console.log(receipt);
+        resolve();
+      })
+      .catch((err) => reject(err));
+  });
+}
+
+async function sellToken(){
+  const allowance = await tokenInst.methods.allowance(user, dexAddr).call();
+  if(parseInt(finalInput) > parseInt(allowance)){
+    try {
+      await tokenInst.methods.approve(dexAddr, finalInput).send();
+    } catch (err){
+      throw(err);
+    }
+  }
+
+  try {
+    const tokenAddr = tokenInst._address;
+    const sellTx = await dexInst.methods
+      .sellToken(tokenAddr, finalInput, finalOutput)
+      .send();
+    console.log(sellTx);
+  } catch (err) {
+    throw (err);
+  }
+
 }
